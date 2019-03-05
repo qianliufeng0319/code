2e8ebffd214cae321ba1a82514378ac630759572
ccao-gd-china <ccao@godaddy.com>
2019-03-05 22:38:01 +0800

    update

---
+++ src/book/triangle.java
@@ -0,0 +1,45 @@
+package book;
+
+import java.util.Scanner;
+
+public class triangle {
+
+
+  public static void main(String[] args) {
+
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please set line number");
+    String n = sc.nextLine();
+    try {
+      triangle(Integer.valueOf(n));
+
+    } catch (Exception e) {
+      System.out.println("input error");
+    }
+
+  }
+
+
+  private static void triangle(int len) {
+
+    for (int i = 0; i <= len; i++) {
+
+      for (int j = 0; j < i; j++) {
+
+        System.out.print(" ");
+
+      }
+
+      for (int k = 0; k <= (5 - i) * 2; k++) {
+
+        System.out.print("*");
+      }
+
+      System.out.println(" ");
+    }
+  }
+
+
+}
+
