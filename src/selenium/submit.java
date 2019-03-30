94571cff7f85dc79ef01733a1cd6be74ce05892c
qianliufeng0319 <562588154@qq.com>
2019-03-30 15:38:32 +0800

    update

--- src/selenium/submit.java
+++ src/selenium/submit.java
@@ -1,19 +1,30 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class openChrome {
+public class submit {
+	
 	public static void main(String[] args) {
+
+		
 		System.setProperty("webdriver.chrome.driver", "D:/chrome driver/chromedriver.exe");
 		WebDriver dr= new ChromeDriver();
 		dr.get("http://www.baidu.com");
-		try {
+		
+		dr.findElement(By.id("kw")).clear();
+		dr.findElement(By.id("kw")).sendKeys("°×³Õ");
+		dr.findElement(By.id("su")).click();
+		
+		
+  	    try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		dr.quit();
 	}
 }
