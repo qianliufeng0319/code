8744bfb9ed1befee41c622589a0a1d0917f64dc2
qianliufeng0319 <562588154@qq.com>
2019-04-01 15:25:07 +0800

    update

---
+++ src/selenium/WaitStype.java
@@ -0,0 +1,26 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitStype {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:/chrome driver/chromedriver.exe");
+		
+		WebDriver dr = new ChromeDriver();
+		dr.get("http://www.baidu.com");
+		//ÏÔÊ¾µÈ´ý
+		WebElement element  = (new WebDriverWait(dr,5)).until(ExpectedConditions.presenceOfElementLocated(By.id("kw")));
+		
+	
+		
+		
+	}
+
+}
